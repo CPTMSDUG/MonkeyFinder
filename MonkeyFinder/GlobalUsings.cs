b5bf667f571356e3c1aee5d4bcc61ea293809b16
Allan Pead <adpead@gmail.com>
2022-09-10 01:28:11 +0200

    Removed Community Toolkit

--- MonkeyFinder/GlobalUsings.cs
+++ MonkeyFinder/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.Input;
-global using MonkeyFinder.Model;
+﻿global using MonkeyFinder.Model;
 global using MonkeyFinder.ViewModel;
 global using System.Collections.ObjectModel;
 global using System.ComponentModel;
